3d2c0055da24ec33486a1a01b154305d5ea55c16
Jesse Horne <j.horne2796@gmail.com>
2015-10-30 10:51:45 -0400

    Added two lines, different colors, and mouse intersect raycasting which changes the shapes color

--- spiralcubes/js/app.js
+++ spiralcubes/js/app.js
@@ -1,6 +1,6 @@
 var require = function(path) {
     var script = document.createElement('script');
-    script.async = false;
+    script.async = true;
     script.src = path;
     document.getElementsByTagName('body')[0].appendChild(script);
 }
@@ -11,6 +11,9 @@ window.onload = function() {
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+    var raycaster = new THREE.Raycaster();
+    var mouse = new THREE.Vector2();
+
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
@@ -19,30 +22,46 @@ window.onload = function() {
     var cubes = [];
 
     var interval = setInterval(function() {
-        if (cubes.length == 200) {
+        if (cubes.length == 500) {
             clearInterval(interval);
         }
 
-        var cube = new BasicCube(scene);
+        var cube = new BasicCube(scene, "right", 0x550055, .3);
         cubes.push(cube);
 
-    }, 200);
+        var cube = new BasicCube(scene, "left", 0x663322, .3);
+        cubes.push(cube);
+
+
+    }, 100);
 
     camera.position.z = 5;
+
+    function onMouseMove(event) {
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    }
 
     function render() {
         requestAnimationFrame(render);
 
+        raycaster.setFromCamera(mouse, camera);
+        var intersects = raycaster.intersectObjects(scene.children);
+        console.log(intersects);
+        for (var i = 0; i < intersects.length; i++) {
+            intersects[i].object.material.color.set(0xffffff);
+        }
+
         for (var i = 0; i < cubes.length; i++) {
             cubes[i].update();
-            if (cubes[i].mesh.position.z > 5) {
+            if (cubes[i].mesh.position.z > 20 || cubes[i].mesh.position.z < -100) {
                 cubes.splice(i, 1);
                 cubes[i].remove();
                 console.log("removed cube");
             }
         }
 
-        // console.log(cubes.length)
+        window.addEventListener("mousemove", onMouseMove, false);
 
         renderer.render(scene, camera);
     }
--- spiralcubes/js/objects/BasicCube.js
+++ spiralcubes/js/objects/BasicCube.js
@@ -1,28 +1,50 @@
-BasicCube = function(scene) {
-    this.geometry = new THREE.BoxGeometry(.3, .3, .3);
+BasicCube = function(scene, direction, color, size) {
+    this.geometry = new THREE.BoxGeometry(size, size, size);
+
     this.material = new THREE.MeshBasicMaterial({
-        color: 0x447788
+        color: color
     });
 
     this.mesh = new THREE.Mesh(this.geometry, this.material);
-    this.mesh.position.x = 1;
-    this.mesh.position.y = 1;
+    this.mesh.position.x = 0;
+    this.mesh.position.y = 0;
+    this.mesh.position.z = -50;
 
     this.angle = 0;
     this.radius = 2;
+    this.direction = direction;
+    this.zSpeed = .5
+    this.cosSpeed = 10;
+    this.sinSpeed = 10;
 
     scene.add(this.mesh);
 
     this.update = function() {
-        this.angle = (this.angle + .5) % 360;
 
-        this.mesh.position.x = this.radius * Math.cos(10 * Math.PI * this.angle / 360.0);
-        this.mesh.position.y = this.radius * Math.sin(10 * Math.PI * this.angle / 360.0);
+        if (this.direction == "right") {
+            this.angle = (this.angle + .5) % 360;
+            this.mesh.position.x = this.radius * Math.cos(-this.cosSpeed * Math.PI * this.angle / 360.0);
+            this.mesh.position.y = this.radius * Math.sin(-this.sinSpeed * Math.PI * this.angle / 360.0);
+        } else if (this.direction == "long-right") {
+            this.angle = (this.angle + .3) % 360;
+            this.mesh.position.x = this.radius * Math.cos(-this.cosSpeed / 2 * Math.PI * this.angle / 360.0);
+            this.mesh.position.y = this.radius * Math.sin(-this.sinSpeed / 2 * Math.PI * this.angle / 360.0);
+        } else if (this.direction == "left") {
+            this.angle = (this.angle + .5) % 360;
+            this.mesh.position.x = this.radius * Math.cos(this.cosSpeed * Math.PI * this.angle / 360.0);
+            this.mesh.position.y = this.radius * Math.sin(this.sinSpeed * Math.PI * this.angle / 360.0);
+        } else if (this.direction == "long-left") {
+            this.angle = (this.angle + .3) % 360;
+            this.mesh.position.x = this.radius * Math.cos(this.cosSpeed / 2 * Math.PI * this.angle / 360.0);
+            this.mesh.position.y = this.radius * Math.sin(this.sinSpeed / 2 * Math.PI * this.angle / 360.0);
+        }
 
-        this.mesh.position.z += .01;
+        this.zSpeed -= .002
 
-        this.mesh.rotation.x += 0.1;
-        this.mesh.rotation.y += 0.1;
+        this.mesh.position.z += this.zSpeed;
+
+        //this.mesh.rotation.x += 0.1;
+        //this.mesh.rotation.y += 0.1;
     }
 
     this.remove = function() {
